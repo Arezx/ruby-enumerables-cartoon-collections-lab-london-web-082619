58afb7e641b315319728587bfe1bb90e9e287b02
Keginold Philip <keginoldp@hotmail.com>
2019-08-23 14:41:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,28 +1,20 @@
-def roll_call_dwarves(names)# code an argument here
-  # Your code here	 
-  names.each_with_index do |name, index|
-    puts "#{index + 1}. #{name}"
-  end
+def roll_call_dwarves(dwarves)
+  # Your code here	  dwarves.each_with_index {|name, index| puts "#{index + 1} #{name}"}
 end	end
 
 
- def summon_captain_planet# code an argument here	def summon_captain_planet(calls)# code an argument here
-  # Your code here	  
-  calls.collect do |call|
-    "#{call.capitalize}!"
-  end
+ def summon_captain_planet# code an argument here	def summon_captain_planet(arr)
+  # Your code here	  arr.map {|x| "#{x.capitalize}!"}
 end	end
 
 
- def long_planeteer_calls# code an argument here	def long_planeteer_calls(calls)# code an argument here
-  # Your code here	  # Your code here
-  calls.any? { |call| call.length > 4}
+ def long_planeteer_calls# code an argument here	def long_planeteer_calls(arr)
+  # Your code here	  arr.any? {|x| x.length > 4}
 end	end
 
 
- def find_the_cheese# code an argument here	def find_the_cheese(strings)# code an argument here
-  # the array below is here to help	  
+ def find_the_cheese# code an argument here	def find_the_cheese(arr)
+  # the array below is here to help	
   cheese_types = ["cheddar", "gouda", "camembert"]	  cheese_types = ["cheddar", "gouda", "camembert"]
-  strings.find do |string|
-    cheese_types.include?(string)
-  end
+  arr.find {|x| cheese_types.include?(x)}
+end	end
